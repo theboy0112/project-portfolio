2eed8c597d2d28c984ba0612cd612f874050691d
theboy0112 <clifboycabrera12@gmail.com>
2025-07-07 15:24:38 +0800

    portfolio

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -12,16 +12,32 @@ import experience from "./assets/experience.png";
 function Profile() {
   return (
     <div className="row">
-      <div className="column">
-        <img src={boyyy}/>
+      <div className="left-column">
+        <img src={boy} alt="Profile" />
         <h2>Clifbelle Cabrera</h2>
         <h2>Aspiring Frontend Developer</h2>
+        <h3>
+          Aspiring Frontend Developer and 4th year Computer Science student
+          passionate about building responsive, user-friendly web applications.
+          Skilled in HTML, CSS, JavaScript, and React, with a strong foundation
+          in software development and a keen eye for design.
+        </h3>
+      </div>
+
+      <div className="right-column">
+        <div>
+          <img src={education} alt="Education" /> <h1>Education</h1>
+          <ul>
+            <h3>Computer Science Student</h3>
+          </ul>
         </div>
-        <div className="column">
-          <img src={education}/> <p>Computer Science Student</p>
-          <img src={experience}/> <p>Student Intern at Allied Care Experts Medical Center Bohol</p>
-          
-        
+        <br></br>
+        <div>
+          <img src={experience} alt="Experience" /> <h1>Experience</h1>
+          <ul>
+            <h3>Student Intern at Allied Care Experts Medical Center Bohol</h3>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Project.jsx
+++ src/Project.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import sad from "./assets/avatar.jpg";
 import gd from "./assets/gd.jpg";
 import sb from "./assets/sp.jpg";
-import crab from "./assets/crab.png";
+import as from "./assets/as.png";
+import php from "./assets/php.png";
+import bbp from "./assets/bbp.png";
+import react from "./assets/react.png";
+import mysqll from "./assets/mysqll.png";
 import dangerous from "./assets/dangerous.png";
 import "./App.css";
 import { useNavigate } from "react-router-dom";
@@ -15,31 +19,31 @@ function Project() {
         <div className="card-content">
           <h4>Smackies Ordering & Delivery App</h4>
           <h5>
-            Smackies is a convenient and user-friendly food and goods delivery
-            app built using Android Studio. It connects customers with local
-            vendors and delivery riders through a seamless mobile interface. The
-            app uses a MySQL database to handle real-time order tracking,
-            customer profiles, product listings, and delivery status updates.
-            Smackies ensures fast, reliable deliveries with secure order
-            management, ideal for modern on-demand logistics.
+            <p>Android Studio/Frontend</p>
+            <img src={as} />
+            <p>Php/Backend</p>
+            <img src={php} />
+            <p>Mysql/Database</p>
+            <img src={mysqll} />
           </h5>
-          <a href="/contact" className="btn">
-            Contact Us
-          </a>
         </div>
       </div>
       <div className="card">
-        <img className="profile-pic" src={crab} />
+        <img className="profile-pic" src={bbp} />
         <div className="card-content">
           <h4>Boheco Billing</h4>
           <h5>
-            A web-based platform that allows users to conveniently view their
-            BOHECO electricity bill history, including past payments, due dates,
-            and billing amounts for easy tracking and management.
+            <p>React/Frontend</p>
+            <img src={react} />
           </h5>
-          <a href="/contact" className="btn">
-            Contact Us
-          </a>
+          <button
+            style={{ background: "lightblue", color: "blue" }}
+            onClick={() =>
+              window.open("https://boheco-billing.netlify.app/", "_blank")
+            }
+          >
+            More
+          </button>
         </div>
       </div>
       <div className="card">
@@ -47,17 +51,13 @@ function Project() {
         <div className="card-content">
           <h4>Dangerous Object Detection App</h4>
           <h5>
-            This Android Studio-based mobile app is designed for real-time
-            detection of dangerous objects (such as weapons) using advanced
-            image recognition technology. When a threat is detected, the app
-            automatically initiates a phone call to emergency contacts or
-            authorities. It also integrates with a backend database to record
-            which users or companies are using the app and logs who triggered
-            emergency calls.
+            <p>Android Studio/Frontend</p>
+            <img src={as} />
+            <p>Php/Backend</p>
+            <img src={php} />
+            <p>Mysql/Database</p>
+            <img src={mysqll} />
           </h5>
-          <a href="/contact" className="btn">
-            Contact Us
-          </a>
         </div>
       </div>
       <div className="card">
@@ -68,13 +68,14 @@ function Project() {
             Butane Distribution
           </h4>
           <h5>
-            Android and web-based smart Management Information System designed
-            to efficiently manage LPG and butane distribution. Tailored for
-            small to medium enterprises, it offers real-time order tracking,
-            inventory management, delivery scheduling, and customer data
-            insights—streamlining operations across both mobile and desktop
-            platforms for faster, safer, and more reliable gas delivery
-            services.
+            <p>Android Studio/React</p>
+            <img src={as} />
+            &nbsp;
+            <img src={react} />
+            <p>Php/Backend</p>
+            <img src={php} />
+            <p>Mysql/Database</p>
+            <img src={mysqll} />s
           </h5>
           <a href="/contact" className="btn">
             Contact Us
